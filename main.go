818e2f91c5ba8c86039c99fc51884c43f9744add
Lu <pair+kexin.lu@beanworks.com>
2016-03-14 12:01:13 -0700

    support dynamic host now

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+	"flag"
 )
 
 // define templateHandler conform to http.Handler
@@ -19,17 +20,19 @@ type templateHandler struct {
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+		t.templ.Execute(w, r)
 	})
-
-	t.templ.Execute(w, nil)
 }
 
 func main() {
+    var addr = flag.String("addr", ":8080", "The Address of the Application")
+    flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting chat server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 
